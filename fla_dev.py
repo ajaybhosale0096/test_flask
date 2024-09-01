dd422ff980fc752df9ec2585004ad96834df4f24
ajaybhosale0096 <ajaybhosale0096@live.com>
2024-09-01 19:09:49 +0530

    testcom

---
+++ fla_dev.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Ajay, Test-1 app !'
+
+if __name__ == '__main__':
+    # Change the port to your desired port number
+    port = 8081
+    app.run(host='0.0.0.0', port=port)
