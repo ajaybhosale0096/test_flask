c2a1cd29f7f57792a5a8bcbcac0c9aef542ba97a
ajaybhosale0096 <ajaybhosale0096@live.com>
2024-09-17 13:47:30 +0530

    first commit from office

--- fla_dev.py
+++ fla_dev.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return 'Ajay, Test-1 app !'
+    return 'Ajay, Test-1 app !\n office'
+
 
 if __name__ == '__main__':
     # Change the port to your desired port number
